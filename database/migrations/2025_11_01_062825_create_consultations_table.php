fa5744ca69585f0a6fa9e09c9c7edc6daac04138
Naya Salha <salhanaya8@gmail.com>
2025-11-02 12:56:13 +0300

    add getConsultations for admin

--- database/migrations/2025_11_01_062825_create_consultations_table.php
+++ database/migrations/2025_11_01_062825_create_consultations_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('stripe_session_id')->nullable();
             $table->decimal('amount', 10, 2);
             $table->string('meet_url')->nullable();
+            $table->boolean('is_done')->default(false);
             $table->date('consultation_date')->nullable();
             $table->time('consultation_time')->nullable();
             $table->timestamps();
